1a44dc2afc4b9c4c05da8f1412ff9a62292dfc7d
shreq <shrek.kresh@o2.pl>
2020-04-18 19:12:19 +0200

    more output

--- zad2/main.py
+++ zad2/main.py
@@ -1,36 +1,53 @@
+import numpy
 import pandas
 
 
-def na_fraction(dataframe):
-    return dataframe.isna().sum().sum() / dataframe.count().sum()
+def na_fraction(df):
+    return df.isna().sum().sum() / df.count().sum()
 
 
-def get_stats(dataframe):
+def get_stats(df):
     stats = pandas.DataFrame()
-    stats['Mean'] = dataframe.mean()
-    stats['Standard deviation'] = dataframe.std()
-    stats['Q1 - 25%'] = dataframe.quantile(0.25)
-    stats['Q2 - 50%'] = dataframe.quantile(0.5)
-    stats['Q3 - 75%'] = dataframe.quantile(0.75)
+    stats['Mean'] = df.mean()
+    stats['Std deviation'] = df.std()
+    stats['Q1 - 25%'] = df.quantile(0.25)
+    stats['Q2 - 50%'] = df.quantile(0.5)
+    stats['Q3 - 75%'] = df.quantile(0.75)
+    stats['NA%'] = df.isna().sum()
     return stats
+
+
+def impute_mean(df, inplace=False):
+    return df.fillna(df.mean(), inplace=inplace)
 
 
 pandas.options.display.float_format = '{:.2f}'.format
 data = pandas.read_csv('./datasets/horse.csv')
 
+data = data.iloc[:, data.applymap(numpy.isreal).all().values]
 data.drop(
     ['nasogastric_reflux_ph',
-     'abdomo_protein',
-     'abdomo_appearance',
-     'abdomen',
-     'nasogastric_reflux',
-     'nasogastric_tube',
-     'rectal_exam_feces'],
+     'abdomo_protein'],
     axis=1,
     inplace=True
 )
 
+previous = get_stats(data)
 print(
+    '- - - - - Before mean imputation - - - - -\n' +
     'Missing values:\t' + '{:.2%}'.format(na_fraction(data)) + '\n' +
     get_stats(data).T.to_string()
+)
+
+impute_mean(data, inplace=True)
+
+print(
+    '- - - - - After mean imputation - - - - -\n' +
+    'Missing values:\t' + '{:.2%}'.format(na_fraction(data)) + '\n' +
+    get_stats(data).T.to_string()
+)
+
+print(
+    '- - - - - Diff - - - - -\n' +
+    (get_stats(data) - previous).T.to_string()
 )
