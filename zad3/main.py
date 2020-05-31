3d948c8828dbfed7ea6a63bea30fee8fc4f2cd99
Bartek Jurczewski <bartek.jurczewski@gmail.com>
2020-05-31 15:55:27 +0200

    dataset fix

--- zad3/main.py
+++ zad3/main.py
@@ -10,13 +10,12 @@ labels = data["species"]
 data.drop(data.columns[-1], axis=1, inplace=True)
 
 for dataset in generateDatasets(data, labels):
-    print(dataset)
     for training_percent in range(60, 91, 5): 
-        classifier = Bayes(data, labels, training_percent, dataset.name)
+        classifier = Bayes(dataset.data, labels, training_percent, dataset.name)
         classifier.train()
         classifier.test()
         dataset.result.append(classifier.get_accuracy())
-        #print('Training percent: ' + str(training_percent) + '%, accuracy: ' + str(classifier.get_accuracy()))
+        print('Training percent: ' + str(training_percent) + '%, accuracy: ' + str(classifier.get_accuracy()))
     print(dataset.result)
     pyplot.plot(range(60, 91, 5), dataset.result, label=classifier.name)
 
