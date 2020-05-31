7b87ad75b43d92963fb06df94b8f17d652f89f63
Bartek Jurczewski <bartek.jurczewski@gmail.com>
2020-05-31 14:02:50 +0200

    plot

--- zad3/main.py
+++ zad3/main.py
@@ -1,25 +1,28 @@
 import pandas
 from matplotlib import pyplot
+import numpy
 
 from bayes import Bayes
-from utils import PCA, normalize
-from sklearn.datasets import load_breast_cancer
+from utils import pca, normalize, generateDatasets
 
 data = pandas.read_csv('./datasets/leaf.csv')
 labels = data["species"]
 data.drop(data.columns[-1], axis=1, inplace=True)
 
+for dataset in generateDatasets(data, labels):
+    print(dataset)
+    for training_percent in range(60, 91, 5): 
+        classifier = Bayes(data, labels, training_percent, dataset.name)
+        classifier.train()
+        classifier.test()
+        dataset.result.append(classifier.get_accuracy())
+        #print('Training percent: ' + str(training_percent) + '%, accuracy: ' + str(classifier.get_accuracy()))
+    print(dataset.result)
+    pyplot.plot(range(60, 91, 5), dataset.result, label=classifier.name)
 
-for training_percent in range(60, 91, 5):
-    classifier = Bayes(data, labels, training_percent)
-    classifier.train()
-    classifier.test()
-    print(
-        classifier.get_accuracy()
-    )
-
-# pca_data = PCA(normalize(data))
-
+pyplot.xlabel('Training percent')
+pyplot.ylabel('Accuracy')
+pyplot.legend()
 pyplot.savefig(
     'plot',
     dpi=200,
